0fad58f0c01d4d7d0f275275c20116085ecf9352
LouisLao <louiselao64@gmail.com>
2021-12-11 19:54:50 -0800

    Food Order

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from './Header.module.css';
+import mealsIMG from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props =>{
+    return <React.Fragment>
+        <header className={classes.header} >
+            <h2>Food Order</h2>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['.main-image']} >
+            <img src={mealsIMG}/>
+        </div>
+    </React.Fragment>
+};
+
+export default Header;
---
+++ src/components/Meals/AvaliableMeals.js
@@ -0,0 +1,34 @@
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+
+  /* for now 我们用的是dummy data above we need to change it into jsx code */
+const AvaliableMeals = () =>{
+
+};
+
+export default AvaliableMeals;
